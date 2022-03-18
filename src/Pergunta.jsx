d538c0e684792a6d99676b548bfafda4467f12cd
Abner da Silva Tavares <abnerdasilvatavares@gmail.com>
2022-03-18 16:50:13 -0300

    Versão 2

--- src/Pergunta.jsx
+++ src/Pergunta.jsx
@@ -1,15 +1,32 @@
 import { useState } from "react"
-
+import PerguntaTaxada from "./PerguntaTaxada";
+import Resposta from "./Resposta";
+const icons = ["close-circle-sharp","help-circle-sharp","checkmark-circle-sharp"];
 
 
 export default function Pergunta(props) {
-    const {id, pergunta, resposta} = props;
+    const {id, pergunta, resposta, callback, adicionaIcones} = props;
     const [ virada, setVirada ] = useState("virada");
+    // const [memoria, setMemoria] = useState("");
+
+    function selecionaMemoria(str){
+        console.log(str);
+        if(str === "Não lembro"){
+            setVirada("NLembrei");
+            callback();
+        }else if(str === "quase não lembro"){
+            setVirada("QNLembrei");
+            callback();
+        }else if(str === "zap"){
+            setVirada("Zap");
+            callback();
+        }
+    }
 
     if (virada === "virada") {
         return (
-            <div className="carta-virada">
-                <p onClick={() => setVirada("pergunta")}>Pergunta {id}</p>
+            <div onClick={() => setVirada("pergunta")} className="carta-virada">
+                <p>Pergunta {id}</p>
                 <ion-icon name="play-outline"></ion-icon>
             </div>
         )
@@ -21,21 +38,21 @@ export default function Pergunta(props) {
             </div>
         )
     } else if(virada === "resposta")  {
+
+        return(
+            <Resposta selecionaMemoria={selecionaMemoria} resposta={resposta} adicionaIcones={adicionaIcones}/>
+        )
+    }else if(virada === "NLembrei"){
+        return(
+            <PerguntaTaxada estado="errado" icons={icons[0]} id={id}/>
+        )
+    } else if(virada === "QNLembrei"){
+        return(
+            <PerguntaTaxada estado="meio" icons={icons[1]} id={id}/>
+        )
+    } else if(virada === "Zap"){
         return (
-            <div className="resposta">
-                <h2>{resposta}</h2>
-                <div className="memoria">
-                    <div className="nao-lembro">
-                        Não lembrei
-                    </div>
-                    <div className="quase-n-lembro">
-                        Quase não lembrei
-                    </div>
-                    <div className="zap">
-                        Zap!
-                    </div>
-                </div>
-            </div>
+            <PerguntaTaxada estado="zap" icons={icons[2]} id={id}/>
         )
     }
 }
--- src/TelaDeck.jsx
+++ src/TelaDeck.jsx
@@ -1,18 +1,31 @@
 import Pergunta from "./Pergunta"
-
+import { useState } from "react"
 
 const perguntas = [
-    { pergunta: "O que é JSX?", resposta: "Uma extensão de linguagem do JavaScript", id: 1 },
-    { pergunta: "O React é __", resposta: " uma biblioteca JavaScript para construção de interfaces", id: 2 },
-    { pergunta: "Componentes devem iniciar com __", resposta: "letra maiúscula", id: 3 },
-    { pergunta: "Podemos colocar __ dentro do JSX", resposta: "expressões", id: 4 },
-    { pergunta: "O ReactDOM nos ajuda __", resposta: "interagindo com a DOM para colocar componentes React na mesma", id: 5 },
-    { pergunta: "Usamos o npm para __", resposta: "gerenciar os pacotes necessários e suas dependências", id: 6 },
-    { pergunta: " Usamos props para __ ", resposta: "passar diferentes informações para componentes", id: 7 },
-    { pergunta: "Usamos estado (state) para __", resposta: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente", id: 8 }
+    { pergunta: "O que é JSX?", resposta: "Uma extensão de linguagem do JavaScript", id: "1" },
+    { pergunta: "O React é __", resposta: " uma biblioteca JavaScript para construção de interfaces", id: "2" },
+    { pergunta: "Componentes devem iniciar com __", resposta: "letra maiúscula", id: "3" },
+    { pergunta: "Podemos colocar __ dentro do JSX", resposta: "expressões", id: "4" },
+    { pergunta: "O ReactDOM nos ajuda __", resposta: "interagindo com a DOM para colocar componentes React na mesma", id: "5" },
+    { pergunta: "Usamos o npm para __", resposta: "gerenciar os pacotes necessários e suas dependências", id: "6" },
+    { pergunta: " Usamos props para __ ", resposta: "passar diferentes informações para componentes", id: "7" },
+    { pergunta: "Usamos estado (state) para __", resposta: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente", id: "8" }
 ]
 
 export default function TelaDeck() {
+    const [qtd, setQtd] = useState(0);
+    const icones = [];
+    const [icone, setIcone] = useState(icones);
+    // const [respondido,setRespondido] = useState(false);
+
+
+    function adicionaIcones(str) {
+        // icones.push(str);
+        // console.log(icones);
+        // console.log(icone);
+        setIcone([...icone, str]);
+    }
+
     return (
         <>
             <header>
@@ -22,10 +35,18 @@ export default function TelaDeck() {
             <main>
                 {
                     perguntas.map(question =>
-                        <Pergunta key={question.id} h1={question.pergunta} h2={question.resposta} />
+                        <Pergunta key={question.id} pergunta={question.pergunta} resposta={question.resposta} id={question.id} adicionaIcones={adicionaIcones} callback={() => setQtd(qtd + 1)} />
                     )
                 }
             </main>
+            <footer>
+                <h1>{qtd}/8 CONCLUÍDOS</h1>
+                <div className="icones">
+                    {icone.map((icon, index) =>
+                        <ion-icon className={`${icon}` } key={icon + index} name={icon}></ion-icon>
+                    )}
+                </div>
+            </footer>
         </>
     )
 }
